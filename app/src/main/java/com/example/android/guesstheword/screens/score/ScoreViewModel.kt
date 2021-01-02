f39e68bb71cec89ce4ed73025e397032b17f9915
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2021-01-01 20:55:59 -0500

    Se agrega LiveData a ScoreViewModel.kt

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -4,13 +4,17 @@ package com.example.android.guesstheword.screens.score
  * ViewModel con constructor parametrizado. Esto quiere dcir que recibe un parámetro el cual es implementado en el ViewModel Factory
  */
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int): ViewModel() {
 
-    var score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
 
     init {
-        Log.i("ScoreViewModel", "Final score is $finalScore")
+        _score.value = finalScore
     }
 }
